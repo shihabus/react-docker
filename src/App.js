6cea6efbab78270cc697e6c09ef77c2c74d85569
Shihab US <shihabudheen@bounceshare.com>
2020-02-19 22:41:01 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <p style={{ color: "red" }}>Shihab</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
